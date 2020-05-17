ff7371326585b8df35e1a61d2645f8ce9e9275f7
Caleb Dinsmore <calebjdinsmore@gmail.com>
2020-05-17 18:45:25 -0400

    Fixed Pit-Hag's missing night text

--- rooms/game-room/schemas/characters/bad-moon-rising/lunatic.ts
+++ rooms/game-room/schemas/characters/bad-moon-rising/lunatic.ts
@@ -9,9 +9,7 @@ export class Lunatic extends Character {
       new ReminderToken('Attack 1', 'was attacked 1st by the Lunatic.', this),
       new ReminderToken('Attack 2', 'was attacked 2nd by the Lunatic.', this),
       new ReminderToken('Attack 3', 'was attacked 3rd by the Lunatic.', this),
-      new ReminderToken('Decoy', 'is a decoy Minion (Lunatic)', this),
-      new ReminderToken('Decoy', 'is a decoy Minion (Lunatic)', this),
-      new ReminderToken('Decoy', 'is a decoy Minion (Lunatic)', this),
+      new ReminderToken('Decoy', 'is a decoy Minion (Lunatic)', this, true),
     ];
   }
 }
